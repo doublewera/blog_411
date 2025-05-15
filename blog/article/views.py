899cf8d4b9824d66b1c8496b0104ca4e5762a9e6
Wera Barinova <alisawera@gmail.com>
2025-05-15 19:46:38 +0300

    Передача данных в шаблон

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 def article(request):
+    context = {
+        'title': 'Веселенький заголовочек',
+        'text':  'Интересный рассказ',
+    }
     return render(
         request,
-        'article/page.html'
+        'article/page.html',
+        context
     )
