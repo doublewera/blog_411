48d3803baac1e3cfec2887ef8d348d78fc8c232d
Wera Barinova <alisawera@gmail.com>
2025-05-29 21:42:56 +0300

    Выход с сайта (кнопка-ссылка в меню)

--- blog/mainpage/urls.py
+++ blog/mainpage/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.main, name='mainpage'),
     path('feed/', views.feed, name='feed'),
     path('howto/', views.summary, name='summary'),
-    path('login/', auth_views.LoginView.as_view(template_name='user/login.html')),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='/'), name='logout'),
 ]
