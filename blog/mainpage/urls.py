b2de7558fcf0bfac5b1489b74a25e436002c383e
Wera Barinova <alisawera@gmail.com>
2025-05-29 20:36:49 +0300

    Логин на сайт родными простыми средствами Django

--- blog/mainpage/urls.py
+++ blog/mainpage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.main, name='mainpage'),
     path('feed/', views.feed, name='feed'),
     path('howto/', views.summary, name='summary'),
-    path('login/', views.loginme, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html')),
 ]
--- blog/mainpage/views.py
+++ blog/mainpage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 def main(request):
     return render(
@@ -28,28 +28,3 @@ def summary(request):
         'mainpage/summary.html',  # путь к шаблону
         # здесь будут данные!
     )
-
-
-from . import forms
-from django.contrib import auth
-def loginme(request):
-    if request.method == 'POST':
-        user_form = forms.UserLoginForm(request.POST)
-        if user_form.is_valid():
-            cd = user_form.cleaned_data
-            user = auth.authenticate(
-                username=cd['username'],
-                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    auth.login(request, user)
-                return redirect('/')
-            return redirect('login')
-    else:
-        user_form = forms.UserLoginForm()
-    return render(
-        request,
-        'user/login.html',
-        {
-            'user_form': user_form
-        })
