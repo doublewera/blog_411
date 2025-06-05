9af61fc4b9c5ce69bb4c54d7509dcc4679114c8f
Wera Barinova <alisawera@gmail.com>
2025-06-05 20:15:37 +0300

    Скрипт научился посылать get-запрос и получать ответ

--- blog/mainpage/static/mainpage/script.js
+++ blog/mainpage/static/mainpage/script.js
@@ -1,1 +1,16 @@
 console.log('Работает!')
+let url_addr = '/';
+fetch(url_addr).then( 
+    // СЮДА надо передать ФУНКЦИЮ, которая ПОЛУЧИТ ОТВЕТ
+    (response) => {
+        console.log(
+            'Ура! Сервер ответил на ', url_addr,
+            response.status,
+            response)
+        return response.text()
+    }
+).then(
+    (data) => {
+        console.log(data)
+    }
+)
